9f2e36dd900ea253facae819c6d6110d517b8390
jtfairbank <jtfairbank@gmail.com>
2018-11-10 05:20:36 -0600

    Add IntercoolerJS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,13 +12,15 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import "jquery"
+import "intercooler"
 
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-import kube from "./kube"
+import kube from "./kube-7.2.0"
 // import socket from "./socket"
 
 $K.init({
