843a837eb730be35b5edbe4bc69f8651e313eeaa
Taylor Fairbank <taylor@distributeaid.org>
2018-11-28 01:40:31 -0600

    Initial setup for GitLab CI/CD Pipelines.

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :ferry, Ferry.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "local_dev",
+  username: "toolbox",
   password: "1312",
-  database: "ferry_dev",
+  database: "toolbox_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :ferry, FerryWeb.Endpoint,
-  http: [port: 1313],
+  http: [port: 1314],
   server: false
 
 # Print only warnings and errors during test
@@ -12,10 +12,10 @@ config :logger, level: :warn
 # Configure your database
 config :ferry, Ferry.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "local_dev",
-  password: "1312",
-  database: "ferry_test",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USER") || "toolbox",
+  password: System.get_env("POSTGRES_PASSWORD") || "1312",
+  database: System.get_env("POSTGRES_DB") || "toolbox_test",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # Reduce the # of password hashing rounds to speed up the test suite.
