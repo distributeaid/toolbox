d98b4279274479072ba355713ac0ee838307bc9a
Matthew <walshmjp@gmail.com>
2019-03-31 18:10:05 +0200

    update env variables

--- config/test.exs
+++ config/test.exs
@@ -12,10 +12,10 @@ config :logger, level: :warn
 # Configure your database
 config :ferry, Ferry.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER") || "toolbox",
-  password: System.get_env("POSTGRES_PASSWORD") || "1312",
-  database: System.get_env("POSTGRES_DB") || "toolbox_test",
-  hostname: System.get_env("POSTGRES_HOST") || "localhost",
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_DATABASE"),
+  hostname: System.get_env("DB_HOSTNAME"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # Reduce the # of password hashing rounds to speed up the test suite.
