5f3aee3bde0734d95bfa0fc33020f212f1e69425
sfelder <samfelder17@gmail.com>
2019-04-13 16:28:47 -0700

    tests not working

--- lib/ferry_web/views/error_view.ex
+++ lib/ferry_web/views/error_view.ex
@@ -6,27 +6,28 @@ defmodule FerryWeb.ErrorView do
   #   "Internal Server Error"
   # end
 
+  # explicit check for bad group id as opposed to other 404 errors
   defp check_bad_group_id([views,groups | tail]) when tail != [] do
     [id | tail] = tail
-    cond do
-      views=="public" and groups=="groups" and Integer.parse(id) != :error ->
-        true
-      true ->
-        false
+    if views=="public" and groups=="groups" and Integer.parse(id) != :error do
+      {:ok, "A group with the ID #{id} does not exist"}
+    else
+      {:error, "Page Not Found"}
     end
   end
 
+  # function stubbed out for now, but will allow us to expand 404 error messages as we see fit
   defp check_bad_group_id(_path) do
-    false
+    {:error, "Page Not Found"}
   end
 
+  # custom 404 message
   def render("404.html", %{conn: %{path_info: path }}) do
-    
     case check_bad_group_id(path) do
-      true ->
-        "A group with the ID #{Enum.at(path,2)} does not exist"
-      false ->
-        "Page not Found"
+      {:ok, id} ->
+        id
+      {:error, reason} ->
+        reason
     end
   end
 
--- test/ferry_web/views/error_view_test.exs
+++ test/ferry_web/views/error_view_test.exs
@@ -4,9 +4,13 @@ defmodule FerryWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "check_bad_group_id /groups/hello " do
+    assert {:error, "Page Not Found"} == check_bad_group_id("/groups/hello")
+  end
+
   test "renders 404.html" do
     assert render_to_string(FerryWeb.ErrorView, "404.html", []) ==
-           "Not Found"
+           "Page Not Found"
   end
 
   test "renders 500.html" do
