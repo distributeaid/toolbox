ce6b097b9e8af86e5ddc4192bb71aff34af50494
sfelder <samfelder17@gmail.com>
2019-04-27 09:23:54 -0700

    front end paramaterized to whatever group is accessing shipping routes

--- lib/ferry_web/controllers/shipment_controller.ex
+++ lib/ferry_web/controllers/shipment_controller.ex
@@ -4,57 +4,57 @@ defmodule FerryWeb.ShipmentController do
   alias Ferry.Shipments
   alias Ferry.Shipments.Shipment
 
-  def index(conn, _params) do
+  def index(%{assigns: %{current_user: %{group_id: group_id}}} = conn, _params) do
     shipments = Shipments.list_shipments()
-    render(conn, "index.html", shipments: shipments)
+    render(conn, "index.html", group: group_id, shipments: shipments)
   end
 
   def new(conn, _params) do
     changeset = Shipments.change_shipment(%Shipment{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", group: conn.assigns.current_user.group_id, changeset: changeset)
   end
 
-  def create(conn, %{"shipment" => shipment_params}) do
+  def create(%{assigns: %{current_user: %{group_id: group_id}}} = conn, %{"shipment" => shipment_params}) do
     case Shipments.create_shipment(shipment_params) do
       {:ok, shipment} ->
         conn
         |> put_flash(:info, "Shipment created successfully.")
         |> redirect(to: group_shipment_path(conn, :show, 1, shipment))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", group: group_id, changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(%{assigns: %{current_user: %{group_id: group_id}}} = conn, %{"id" => id}) do
     shipment = Shipments.get_shipment!(id)
-    render(conn, "show.html", shipment: shipment)
+    render(conn, "show.html", group: group_id, shipment: shipment)
   end
 
-  def edit(conn, %{"id" => id}) do
+  def edit(%{assigns: %{current_user: %{group_id: group_id}}} = conn, %{"id" => id}) do
     shipment = Shipments.get_shipment!(id)
     changeset = Shipments.change_shipment(shipment)
-    render(conn, "edit.html", shipment: shipment, changeset: changeset)
+    render(conn, "edit.html", group: group_id, shipment: shipment, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "shipment" => shipment_params}) do
+  def update(%{assigns: %{current_user: %{group_id: group_id}}} = conn, %{"id" => id, "shipment" => shipment_params}) do
     shipment = Shipments.get_shipment!(id)
 
     case Shipments.update_shipment(shipment, shipment_params) do
       {:ok, shipment} ->
         conn
         |> put_flash(:info, "Shipment updated successfully.")
-        |> redirect(to: group_shipment_path(conn, :show, 1, shipment))
+        |> redirect(to: group_shipment_path(conn, :show, group_id, shipment))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", shipment: shipment, changeset: changeset)
+        render(conn, "edit.html", group: group_id, shipment: shipment, changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(%{assigns: %{current_user: %{group_id: group_id}}} = conn, %{"id" => id}) do
     shipment = Shipments.get_shipment!(id)
     {:ok, _shipment} = Shipments.delete_shipment(shipment)
 
     conn
     |> put_flash(:info, "Shipment deleted successfully.")
-    |> redirect(to: group_shipment_path(conn, :index, 1))
+    |> redirect(to: group_shipment_path(conn, :index, group_id))
   end
 end
