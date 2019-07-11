e94bcffae4ce6a4fa937774ab0febf2395e79eb1
Kyle Ryan <kryan717@gmail.com>
2019-07-11 19:30:12 +0200

    changed unique index on the stock items inventory

---
+++ priv/repo/migrations/20190711171453_fix_unique_item_index.exs
@@ -0,0 +1,9 @@
+defmodule Ferry.Repo.Migrations.FixUniqueItemIndex do
+  use Ecto.Migration
+
+  def change do
+    drop index(:inventory_items, [:name])
+    create unique_index(:inventory_items, [:category_id, :name])
+  end
+
+end
