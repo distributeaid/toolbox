6b41c55f620d617200e4f800db45a48abcf64f04
Taylor Fairbank <taylor@distributeaid.org>
2019-09-01 16:45:04 +0200

    Convert the rest of classes from Kube => Spectre and do an initial pass to clean up our stylesheet.

--- lib/ferry_web/views/component_helpers.ex
+++ lib/ferry_web/views/component_helpers.ex
@@ -1,6 +1,5 @@
 # Based on: https://blog.danielberkompas.com/2017/01/17/reusable-templates-in-phoenix/
 defmodule FerryWeb.ComponentHelpers do
-
   def component(template) do
     FerryWeb.ComponentView.render(template)
   end
--- lib/ferry_web/views/layout_helpers.ex
+++ lib/ferry_web/views/layout_helpers.ex
@@ -1,29 +1,2 @@
 defmodule FerryWeb.LayoutHelpers do
-  def page_background_img_class(_conn = %{request_path: request_path}) do
-    cond do
-      String.match?(request_path, ~r/^\/public\/addresses/u) ->
-        "page-content--addresses"
-
-      String.match?(request_path, ~r/^\/public\/groups\/[\d]+\/projects/u) ->
-        "page-content--projects"
-      String.match?(request_path, ~r/^\/public\/projects/u) ->
-        "page-content--projects"
-      String.match?(request_path, ~r/^\/groups\/[\d]+\/projects/u) ->
-        "page-content--projects"
-
-      String.match?(request_path, ~r/^\/public\/groups/u) ->
-        "page-content--groups"
-      String.match?(request_path, ~r/^\/groups/u) ->
-        "page-content--groups"
-
-      String.match?(request_path, ~r/^\/public\/session/u) ->
-        "page-content--session"
-
-      String.match?(request_path, ~r/^\//u) ->
-        "page-content--welcome"
-      true ->
-        "page-content--welcome" # always show a background image
-                                # TODO: pick a distinct default one
-    end
-  end
 end
