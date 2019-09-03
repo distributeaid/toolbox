76d94ecca0f863507ea393a047af2e687ff6fd2d
Taylor Fairbank <taylor@distributeaid.org>
2019-09-03 17:23:34 +0200

    Style inventory lists (all / my).

--- lib/ferry_web/controllers/inventory_list_controller.ex
+++ lib/ferry_web/controllers/inventory_list_controller.ex
@@ -2,16 +2,24 @@ defmodule FerryWeb.InventoryListController do
   use FerryWeb, :controller
 
   alias Ferry.Inventory
+  alias Ferry.Profiles
 
 
   # Inventory Controller
   # ==============================================================================
 
+  # Helpers
+  # ------------------------------------------------------------
+
+  defp current_group(_conn = %{assigns: %{current_user: %{group_id: group_id}}}) do
+    Profiles.get_group!(group_id)
+  end
+
   # Show
   # ------------------------------------------------------------
   def show(conn, _params) do
     inventory = Inventory.get_inventory()
-    render(conn, "show.html", inventory: inventory)
+    render(conn, "show.html", inventory: inventory, current_group: current_group(conn))
   end
 
 end
