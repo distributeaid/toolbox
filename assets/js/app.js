4cc957260793652e4ff84e9fd2c04dc9f753200e
Taylor Fairbank <taylor@distributeaid.org>
2019-09-23 11:39:19 +0200

    Split vendors bundle out to allow caching between app updates.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "spectre.css/dist/spectre.min.css";
-import "spectre.css/dist/spectre-exp.min.css";
+import "spectre.css/dist/spectre.css";
+import "spectre.css/dist/spectre-exp.css";
 import "select2/dist/css/select2.css";
 import "../css/app.css";
 
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env, options) => ({
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
+
   entry: {
-    './js/app.js': glob.sync('./vendor/**/*.min.js').concat(['./js/app.js'])
+    app: glob.sync('./vendor/**/*.min.js').concat(['./js/app.js'])
   },
+
   output: {
-    filename: 'app.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, '../priv/static/js')
   },
+
   module: {
     rules: [
       {
@@ -35,11 +34,26 @@ module.exports = (env, options) => ({
       }
     ]
   },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+
+    // based on: https://medium.com/hackernoon/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new CleanWebpackPlugin(),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+    new MiniCssExtractPlugin({ filename: '../css/[name].css' }),
     new webpack.ProvidePlugin({
       $: 'jquery'
     })
   ]
+
 });
