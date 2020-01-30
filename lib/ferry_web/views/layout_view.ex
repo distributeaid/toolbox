5b75f34c7314fbc46ec49080878d4de14da27dd0
Markus Tacker <m@coderbyheart.com>
2020-01-30 08:38:10 +0100

    Update chat

--- lib/ferry_web/views/layout_view.ex
+++ lib/ferry_web/views/layout_view.ex
@@ -26,7 +26,7 @@ defmodule FerryWeb.LayoutView do
       <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.11.0/umd/react-dom.production.min.js" defer></script>
       <script src="https://media.twiliocdn.com/sdk/js/chat/v3.3/twilio-chat.min.js" defer></script>
       <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" defer></script>
-      <script src="https://github.com/distributeaid/chat-ui/releases/download/v1.14.0/distribute-aid-chat-ui.v1.14.0.js" defer></script>
+      <script src="https://github.com/distributeaid/chat-ui/releases/download/v1.14.1/distribute-aid-chat-ui.v1.14.1.js" defer></script>
       <script type="text/javascript">
         window.onload = (\) => {
           DAChat({
