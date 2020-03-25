4a578a17d7376db34d4a12d2f7993b165be51e31
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-03-26 00:08:25 +0100

    feat(chat): use latest JS version
    While we are developing, use the latest version of
    the library, which is now publish for every commit.

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,75 +1,96 @@
-const chatVersion = '1.17.8'
+// NOTE: this version used the "latest" version of the Chat UI
+// which is under active development.
 
-const loadJsAsync = deps => loadjs(deps, {
+const reactVersion = "16.13.1";
+const twilioChatVersion = "3.3";
+
+const loadJsAsync = deps =>
+  loadjs(deps, {
     async: true,
     returnPromise: true
-})
+  });
 
 const log = (...args) =>
-    console.log(
-        '%cChat',
-        'background-color: #3543ec; color: #ffffff; padding: 0.25rem;',
-        ...args,
-    )
+  console.log(
+    "%cChat",
+    "background-color: #3543ec; color: #ffffff; padding: 0.25rem;",
+    ...args
+  );
 
 const logError = (...args) =>
-    console.error(
-        '%cChat',
-        'background-color: #da2525; color: #ffffff; padding: 0.25rem;',
-        ...args,
-    )
+  console.error(
+    "%cChat",
+    "background-color: #da2525; color: #ffffff; padding: 0.25rem;",
+    ...args
+  );
 
 const loadChat = () => {
-    const context = document.querySelector("meta[name='chat:context']")
-    if (!context) {
-        return Promise.reject('No chat context defined!')
-    }
-    return loadJsAsync([
-        'https://cdnjs.cloudflare.com/ajax/libs/react/16.12.0/umd/react.production.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.11.0/umd/react-dom.production.min.js',
-        'https://media.twiliocdn.com/sdk/js/chat/v3.3/twilio-chat.min.js',
-        'https://twemoji.maxcdn.com/v/latest/twemoji.min.js',
-    ])
-        .then(() => loadJsAsync([
-            `https://github.com/distributeaid/chat-ui/releases/download/v${chatVersion}/distribute-aid-chat-ui.v${chatVersion}.js`,
-        ]))
-        .then(() => {
-            DAChat({
-                context: context.getAttribute("content"),
-                apiKey: document.querySelector("meta[name='chat:apiKey']").getAttribute("content"),
-                apiEndpoint: document.querySelector("meta[name='chat:endpoint']").getAttribute("content"),
-                token: document.querySelector("meta[name='chat:jwt']").getAttribute("content"),
-            })
-        })
-}
+  const context = document.querySelector("meta[name='chat:context']");
+  if (!context) {
+    return Promise.reject("No chat context defined!");
+  }
+  return loadJsAsync([
+    `https://cdnjs.cloudflare.com/ajax/libs/react/${reactVersion}/umd/react.production.min.js`,
+    `https://cdnjs.cloudflare.com/ajax/libs/react-dom/${reactVersion}/umd/react-dom.production.min.js`,
+    `https://media.twiliocdn.com/sdk/js/chat/v${twilioChatVersion}/twilio-chat.min.js`,
+    "https://twemoji.maxcdn.com/v/latest/twemoji.min.js"
+  ])
+    .then(() =>
+      loadJsAsync([
+        `https://distributeaid.github.io/chat-ui/main.js?${Math.round(
+          Date.now() / 100000
+        )}`
+      ])
+    )
+    .then(() => {
+      DAChat({
+        context: context.getAttribute("content"),
+        apiKey: document
+          .querySelector("meta[name='chat:apiKey']")
+          .getAttribute("content"),
+        apiEndpoint: document
+          .querySelector("meta[name='chat:endpoint']")
+          .getAttribute("content"),
+        token: document
+          .querySelector("meta[name='chat:jwt']")
+          .getAttribute("content")
+      });
+    });
+};
 
 window.onload = () => {
-    if (window.localStorage.getItem('DAChat:enabled') === '1') {
-        const token = document.querySelector("meta[name='chat:jwt']")
-        if (!token) {
-            log('Chat disabled: no token present in page.')
-            return
-        }
-        if (document.cookie.split(';').filter((item) => item.trim().startsWith('DAChatopen=')).length) {
-            loadChat()
-                .catch(logError)
-        } else {
-            loadJsAsync([
-                `https://github.com/distributeaid/chat-ui/releases/download/v${chatVersion}/distribute-aid-chat-button.v${chatVersion}.js`
-            ])
-                .then(() => {
-                    DAChatButton((removeButton) => {
-                        document.cookie = 'DAChatopen=1'
-                        loadChat()
-                            .then(() => {
-                                removeButton()
-                            })
-                    })
-                })
-                .catch(logError)
-        }
-    } else {
-        log('Chat disabled. Run this command to enable it for this browser and reload the page:')
-        log(`window.localStorage.setItem('DAChat:enabled', '1')`)
+  if (window.localStorage.getItem("DAChat:enabled") === "1") {
+    const token = document.querySelector("meta[name='chat:jwt']");
+    if (!token) {
+      log("Chat disabled: no token present in page.");
+      return;
     }
-}
+    if (
+      document.cookie
+        .split(";")
+        .filter(item => item.trim().startsWith("DAChatopen=")).length
+    ) {
+      loadChat().catch(logError);
+    } else {
+      loadJsAsync([
+        `https://distributeaid.github.io/chat-ui/chatbutton.js?${Math.round(
+          Date.now() / 100000
+        )}`
+      ])
+        .then(() => {
+          DAChatButton(removeButton => {
+            document.cookie = "DAChatopen=1";
+            loadChat().then(() => {
+              removeButton();
+            });
+          });
+        })
+        .catch(logError);
+    }
+  } else {
+    log(
+      "Chat disabled. Run this command to enable it for this browser and reload the page:"
+    );
+    log(`window.localStorage.setItem('DAChat:enabled', '1')`);
+  }
+};
