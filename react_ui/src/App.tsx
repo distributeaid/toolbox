3aff12f86b090b4526d856a7b0affcd11a409ab0
jefk <jeff@substantial.com>
2020-03-28 15:19:31 -0700

    use amplify to set up user pool

--- react_ui/src/App.tsx
+++ react_ui/src/App.tsx
@@ -1,45 +1,82 @@
 import React from "react";
 import "./App.css";
 import Amplify, { Auth } from "aws-amplify";
+import amplifyConfig from "./aws-exports";
 
-Amplify.configure({
-  Auth: {
-    identityPoolId: "us-east-2_W0AH6xG63", // Amazon Cognito Identity Pool ID
-    region: "us-east-2" // Amazon Cognito Region
-  }
-});
+Amplify.configure(amplifyConfig);
 
 function App() {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   return (
-    <div>
-      Throw this form away
-      <form
-        onSubmit={event => {
-          event.preventDefault();
-          Auth.signIn(username, password)
-            .then(user => console.log(user))
-            .catch(err => console.log(err));
-        }}
-      >
-        <label>
-          Username / email
-          <input
-            name="username"
-            onChange={event => setUsername(event.currentTarget.value)}
-          ></input>
-        </label>
-        <label>
-          Password
-          <input
-            onChange={event => setPassword(event.currentTarget.value)}
-            type="password"
-            name="password"
-          ></input>
-        </label>
-        <input type="submit" value="Sign in" />
-      </form>
+    <div className="grid gap-5 p-5">
+      <h1 className="text-2xl">Throw away sign up / Sign in</h1>
+      <div className="mb">
+        Sign in
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            Auth.signIn(username, password)
+              .then(user => console.log(user))
+              .catch(err => console.log(err));
+          }}
+        >
+          <div className="grid">
+            <label>
+              Email
+              <input
+                className="border"
+                name="username"
+                onChange={event => setUsername(event.currentTarget.value)}
+              ></input>
+            </label>
+            <label>
+              Password
+              <input
+                className="border"
+                onChange={event => setPassword(event.currentTarget.value)}
+                type="password"
+                name="password"
+              ></input>
+            </label>
+
+            <input type="submit" value="Sign in" />
+          </div>
+        </form>
+      </div>
+      <div className="mb-5">
+        Sign up
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            Auth.signUp({ username, password })
+              .then(user => console.log(user))
+              .catch(err => console.log(err));
+          }}
+        >
+          <div className="grid">
+            <label>
+              Email
+              <input
+                className="border"
+                name="username"
+                onChange={event => setUsername(event.currentTarget.value)}
+              ></input>
+            </label>
+            <label>
+              Password
+              <input
+                className="border"
+                onChange={event => setPassword(event.currentTarget.value)}
+                type="password"
+                name="password"
+              ></input>
+            </label>
+
+            <input type="submit" value="Sign up" />
+          </div>
+        </form>
+      </div>
       <div>{username}</div>
       <div>{password}</div>
     </div>
