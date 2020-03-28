43425434de4b8a643a0f8c3acd5762059be96279
jefk <jeff@substantial.com>
2020-03-28 12:15:41 -0700

    throw away signin form

--- react_ui/src/App.tsx
+++ react_ui/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Throw this form away
+      <form>
+        <label>
+          Username / email
+          <input
+            name="username"
+            onChange={event => setUsername(event.currentTarget.value)}
+          ></input>
+        </label>
+        <label>
+          Password
+          <input
+            onChange={event => setPassword(event.currentTarget.value)}
+            type="password"
+            name="password"
+          ></input>
+        </label>
+      </form>
+      <div>{username}</div>
+      <div>{password}</div>
     </div>
   );
 }
