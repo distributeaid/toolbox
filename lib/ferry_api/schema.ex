33cba2bf7ebd0a95f8ee8793ef9d2e6c58e66e83
jefk <jeff@substantial.com>
2020-04-01 11:08:05 -0700

    run formatter

--- lib/ferry_api/schema.ex
+++ lib/ferry_api/schema.ex
@@ -5,17 +5,16 @@ defmodule FerryApi.Schema do
   # Types
   # ------------------------------------------------------------
 
-  import_types Absinthe.Plug.Types
-  import_types FerryApi.Schema.ProfileTypes
+  import_types(Absinthe.Plug.Types)
+  import_types(FerryApi.Schema.ProfileTypes)
 
-  import_types FerryApi.Schema.SessionType
-  import_types FerryApi.Schema.Session
+  import_types(FerryApi.Schema.SessionType)
+  import_types(FerryApi.Schema.Session)
 
   # Queries
   # ------------------------------------------------------------
 
   query do
-
     @desc "Health check"
     field :health_check, :string do
       resolve(fn _parent, _args, _resolution ->
@@ -23,17 +22,14 @@ defmodule FerryApi.Schema do
       end)
     end
 
-    import_fields :group_queries
-    import_fields :session_queries
-
+    import_fields(:group_queries)
+    import_fields(:session_queries)
   end
 
   # Mutuations
   # ------------------------------------------------------------
 
   mutation do
-
-    import_fields :group_mutations
-
+    import_fields(:group_mutations)
   end
 end
--- lib/ferry_api/session_schema.ex
+++ lib/ferry_api/session_schema.ex
@@ -5,18 +5,18 @@ defmodule FerryApi.Schema.Session do
   object :session_queries do
     @desc "Get current users session"
     field :session, :session do
-      arg :access_token, non_null(:string)
-      resolve &get_session/3
+      arg(:access_token, non_null(:string))
+      resolve(&get_session/3)
     end
   end
 
   def get_session(_parent, %{access_token: access_token}, _resolution) do
     access_token
-    |> Ferry.Cognito.get_user
-    |> ExAws.request
+    |> Ferry.Cognito.get_user()
+    |> ExAws.request()
     |> case do
       {:ok, _} -> {:ok, %{id: "me"}}
-      _ -> {:error, message: "Invalid access token", code: Constants.errors.unauthorized}
+      _ -> {:error, message: "Invalid access token", code: Constants.errors().unauthorized}
     end
   end
 end
