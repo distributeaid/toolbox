92761d75a5dd746caa884b36c2cae3013fc1e8d2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-04-08 00:42:58 +0200

    test(e2e): update tests for Amplify UI

--- react_ui/cypress/integration/signup.js
+++ react_ui/cypress/integration/signup.js
@@ -1,19 +1,27 @@
+import { v4 } from 'uuid'
+
 describe('Sign up', () => {
   it('use sign up link', () => {
     cy.visit('/')
+    cy.get('[data-cy=toggle-menu]').click()
     cy.get('[data-cy=sign-up-link]').click()
-    cy.url().should('match', /sign-up/)
+    // This will take us to the sign in form
+    cy.url().should('match', /sign-in/)
+    cy.get('[data-test=sign-in-create-account-link]').click()
   })
-  it('fill form', () => {
-    const emailInput = cy.get('#signup_email')
-    emailInput.type('alex@example.com')
-    const passwordInput = cy.get('#signup_password')
+  it('fill in the form', () => {
+    const emailInput = cy.get(
+      ':nth-child(1) > [data-test=sign-up-non-phone-number-input]'
+    )
+    emailInput.type(`alex-${v4()}@example.com`)
+    const passwordInput = cy.get(
+      ':nth-child(2) > [data-test=sign-up-non-phone-number-input]'
+    )
     passwordInput.type('FooB@r42!')
-    const button = cy.get('[data-cy=signup-button]')
+    const button = cy.get('[data-test=sign-up-create-account-button]')
     button.click()
-    console.log(button)
-    button.should('be.disabled')
-    emailInput.should('be.disabled')
-    passwordInput.should('be.disabled')
+  })
+  it('see Confirm Sign Up page', () => {
+    cy.get('[data-test=confirm-sign-up-confirm-button]').should('exist')
   })
 })
