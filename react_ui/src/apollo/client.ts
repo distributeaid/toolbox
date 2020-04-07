5644681275febbb7facc0e32cbb2be53f2093ef2
jefk <jeff@substantial.com>
2020-04-06 22:03:30 -0700

    get token from cognito store

--- react_ui/src/apollo/client.ts
+++ react_ui/src/apollo/client.ts
@@ -1,13 +1,47 @@
 import ApolloClient from 'apollo-boost'
+import Amplify, { Auth } from 'aws-amplify'
+import amplifyConfig from '../aws-exports'
+import { CognitoUser } from 'amazon-cognito-identity-js'
+
+Amplify.configure(amplifyConfig)
 
 export const client = new ApolloClient({
   uri: '/api/graphiql',
+  // request: async (operation) => {
+  //   let user: CognitoUser | undefined = undefined
+  //   try {
+  //     user = await Auth.currentAuthenticatedUser()
+  //   } catch {
+  //     // not signed in
+  //   }
+  //   if (!user) {
+  //     return
+  //   }
+
+  //   const token = user.getSignInUserSession()?.getAccessToken().getJwtToken()
+
+  //   operation.setContext({
+  //     headers: {
+  //       authorization: token ? `Bearer ${token}` : '',
+  //     },
+  //   })
+  // },
   request: (operation) => {
-    const authToken = JSON.parse(localStorage.getItem('authToken') || 'null')
-    operation.setContext({
-      headers: {
-        authorization: authToken ? `Bearer ${authToken}` : '',
-      },
-    })
+    return Auth.currentAuthenticatedUser()
+      .then((user: CognitoUser) => {
+        const token = user
+          .getSignInUserSession()
+          ?.getAccessToken()
+          .getJwtToken()
+
+        operation.setContext({
+          headers: {
+            authorization: token ? `Bearer ${token}` : '',
+          },
+        })
+      })
+      .catch(() => {
+        // not signed in
+      })
   },
 })
