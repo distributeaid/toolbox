9f9281d1fdb232a3e4db9910de083f6d95c8d926
David Golightly <david.golightly@leapyear.io>
2020-04-06 20:38:39 -0700

    add test for unauthenticated state

--- react_ui/src/App.test.tsx
+++ react_ui/src/App.test.tsx
@@ -1,9 +1,8 @@
 import { render } from '@testing-library/react'
+import { Auth } from 'aws-amplify'
 import React from 'react'
 
 import App from './App'
-import { Auth } from 'aws-amplify'
-import { InMemoryCache } from 'apollo-boost'
 
 jest.mock('aws-amplify')
 jest.mock('aws-amplify-react')
@@ -18,11 +17,27 @@ it('loads authentication state from aws & displays logged in state', async () =>
 
   const { getByText, findByText, queryByText } = render(<App />)
   expect(getByText('Loading...')).toBeVisible()
-  // expect(getByText('Sign up / Sign in')).toBeVisible()
   expect(queryByText('Log out')).toBeNull()
 
   // auth loads
   expect(await findByText('Log out')).toBeVisible()
+
+  // chapter list loads
+  expect(getByText('ChapterList')).toBeVisible()
+})
+
+it('loads authentication state from aws & displays sign in link', async () => {
+  ;(Auth.currentAuthenticatedUser as jest.Mock).mockReturnValueOnce(
+    Promise.reject()
+  )
+
+  const { getByText, findByText, queryByText } = render(<App />)
+  expect(getByText('Loading...')).toBeVisible()
+  expect(queryByText('Log out')).toBeNull()
+
+  // auth loads
+  expect(await findByText('Sign up / Sign in')).toBeVisible()
+  expect(queryByText('Log out')).toBeNull()
 
   // chapter list loads
   expect(getByText('ChapterList')).toBeVisible()
