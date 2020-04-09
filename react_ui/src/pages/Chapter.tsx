b1bb16450c2eaecf0e0d2ee38bacd617626d59e5
David Golightly <david.golightly@leapyear.io>
2020-04-08 19:26:54 -0700

    include a 'back to directory' link

--- react_ui/src/pages/Chapter.tsx
+++ react_ui/src/pages/Chapter.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import { ContentContainer } from '../components/ContentContainer'
 import { useGetChapterQuery } from '../generated/graphql'
+import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 type Props = {
   slug: string
 }
 
 export const Chapter: React.FC<Props> = ({ slug }) => {
+  const { t } = useTranslation()
   const { loading, data } = useGetChapterQuery({ variables: { id: slug } })
 
   if (loading) {
@@ -21,14 +24,17 @@ export const Chapter: React.FC<Props> = ({ slug }) => {
   const { group: chapter } = data
 
   return (
-    <ContentContainer>
-      <div className="p-4 md:p-8">
-        <div className="mb-4 md:mb-8">
-          <h1 className="font-bold text-xl">{chapter.name}</h1>
+    <>
+      <NavLink to="/chapters">{t('chapter.backLink')}</NavLink>
+      <ContentContainer>
+        <div className="p-4 md:p-8">
+          <div className="mb-4 md:mb-8">
+            <h1 className="font-bold text-xl">{chapter.name}</h1>
 
-          <p className="py-4">{chapter.description}</p>
+            <p className="py-4">{chapter.description}</p>
+          </div>
         </div>
-      </div>
-    </ContentContainer>
+      </ContentContainer>
+    </>
   )
 }
