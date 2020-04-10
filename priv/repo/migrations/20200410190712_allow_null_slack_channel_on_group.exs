5cfb54d14a1e31b9b83650cd9ec9b27a3bddc440
bion <bionjohnson@gmail.com>
2020-04-10 14:00:56 -0700

    Make the migration reversible

--- priv/repo/migrations/20200410190712_allow_null_slack_channel_on_group.exs
+++ priv/repo/migrations/20200410190712_allow_null_slack_channel_on_group.exs
@@ -1,11 +1,19 @@
 defmodule Elixir.Ferry.Repo.Migrations.AllowNullSlackChannelOnGroup do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute "ALTER TABLE groups ALTER COLUMN slack_channel_name DROP NOT NULL"
     execute "ALTER TABLE groups ALTER COLUMN request_form_results DROP NOT NULL"
     execute "ALTER TABLE groups ALTER COLUMN volunteer_form_results DROP NOT NULL"
     execute "ALTER TABLE groups ALTER COLUMN donation_form_results DROP NOT NULL"
     execute "ALTER TABLE groups ALTER COLUMN description DROP NOT NULL"
+  end
+
+  def down do
+    execute "ALTER TABLE groups ALTER COLUMN slack_channel_name SET NOT NULL"
+    execute "ALTER TABLE groups ALTER COLUMN request_form_results SET NOT NULL"
+    execute "ALTER TABLE groups ALTER COLUMN volunteer_form_results SET NOT NULL"
+    execute "ALTER TABLE groups ALTER COLUMN donation_form_results SET NOT NULL"
+    execute "ALTER TABLE groups ALTER COLUMN description SET NOT NULL"
   end
 end
