f4877d96018c8e29d495761a2d22e8d2ccbf55ce
Libby Li <libby@leapyear.io>
2020-04-10 23:24:54 -0700

    Add FormInput component

---
+++ react_ui/src/components/FormInput.tsx
@@ -0,0 +1,28 @@
+import { Input } from './Input'
+import React from 'react'
+import { Field, FieldProps, ErrorMessage } from 'formik'
+
+type Props = {
+  name: string
+  title: string
+  type: string
+}
+
+export const FormInput: React.FC<Props> = ({ name, title, type }) => {
+  return (
+    <Field name={name}>
+      {({ field }: FieldProps) => {
+        return (
+          <>
+            <Input {...field} id={name} title={title} type={type} />
+            <ErrorMessage name={name}>
+              {(msg) => (
+                <span className="text-red-600 text-sm -mt-2">{msg}</span>
+              )}
+            </ErrorMessage>
+          </>
+        )
+      }}
+    </Field>
+  )
+}
