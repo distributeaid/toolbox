befc7954c5821cf1239da7c2ede264b0d81c296f
bion <bionjohnson@gmail.com>
2020-04-11 11:56:49 -0700

    Fix group schema test

--- test/ferry_api/group_schema_test.exs
+++ test/ferry_api/group_schema_test.exs
@@ -278,7 +278,7 @@ defmodule Ferry.GroupTest do
             message
           }
           result {
-            id            
+            id
           }
         }
       }
@@ -296,6 +296,10 @@ defmodule Ferry.GroupTest do
           "messages" => [
             %{
               "field" => field,
+              "message" => "can't be blank"
+            },
+            %{
+              "field" => "slug",
               "message" => "can't be blank"
             }
           ],
@@ -323,6 +327,7 @@ defmodule Ferry.GroupTest do
           id: #{group.id},
           groupInput: {
             name: "#{updates.name}",
+            slug: "#{updates.slug}",
             description: "#{updates.description}",
             slackChannelName: "#{updates.slack_channel_name}",
             requestForm: "#{updates.request_form}",
