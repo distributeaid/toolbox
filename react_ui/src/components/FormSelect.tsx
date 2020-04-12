19fc5f5e610d8f002b13322f607941ccaf873c87
Libby Li <libby@leapyear.io>
2020-04-11 23:26:53 -0700

    Add FormSelect component

---
+++ react_ui/src/components/FormSelect.tsx
@@ -0,0 +1,44 @@
+import { Select } from './Select'
+import React from 'react'
+import { Field, FieldProps, ErrorMessage } from 'formik'
+
+type Props = {
+  name: string
+  label: string
+  options: { name: string; value: string }[]
+  value: string
+  includeBlank?: boolean
+}
+
+export const FormSelect: React.FC<Props> = ({
+  name,
+  label,
+  options,
+  value,
+  includeBlank,
+}) => {
+  return (
+    <Field name={name}>
+      {({ field, form: { setFieldValue } }: FieldProps) => {
+        return (
+          <>
+            <Select
+              {...field}
+              id={name}
+              onChange={(evt) => setFieldValue(name, evt.target.value)}
+              label={label}
+              options={options}
+              value={value}
+              includeBlank={includeBlank}
+            />
+            <ErrorMessage name={name}>
+              {(msg) => (
+                <span className="text-red-600 text-sm -mt-2">{msg}</span>
+              )}
+            </ErrorMessage>
+          </>
+        )
+      }}
+    </Field>
+  )
+}
