27e2f17271002d57c4b7cfcd0df976ec3bf0cc19
David Golightly <david.golightly@leapyear.io>
2020-04-12 10:58:47 -0700

    use M4D logo

--- react_ui/src/components/layout/NavBar.tsx
+++ react_ui/src/components/layout/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import { AuthenticationState } from '../../auth/types'
+import { M4DLogo } from './M4DLogo'
 
 type Props = {
   authState: AuthenticationState
@@ -31,20 +32,7 @@ export const NavBar: React.FC<Props> = ({ authState, onSignOut }) => {
       <nav className="flex items-center justify-between flex-wrap bg-gray-900 text-gray-100 py-2 px-3">
         <div className="flex items-center flex-shrink-0 mr-9">
           <NavLink to="/">
-            {/* todo replace with mask for docs logo */}
-            <svg
-              className="fill-current h-8 w-8 mr-2"
-              width="54"
-              height="54"
-              viewBox="0 0 54 54"
-              xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-          </NavLink>
-          <NavLink to="/">
-            <span className="font-semibold text-l tracking-tight">
-              Masks For Docs
-            </span>
+            <M4DLogo />
           </NavLink>
         </div>
         <div className="block lg:hidden">
