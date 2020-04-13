c369e32f8593b42b4cae26a2367a43cdbb2238ce
bion <bionjohnson@gmail.com>
2020-04-12 21:38:12 -0700

    Add placeCodeToName util fn

---
+++ react_ui/src/util/placeCodeToName.ts
@@ -0,0 +1,24 @@
+import countries from '../data/countries.json'
+import usStates from '../data/us_states.json'
+
+type MaybeString = string | undefined
+
+type Place = {
+  code: string
+  name: string
+}
+
+const countryCodeToNameMap: Map<string, string> = new Map()
+
+countries.forEach((country: Place) => countryCodeToNameMap.set(country.code, country.name))
+
+export const countryCodeToName = (code: string): MaybeString =>
+  countryCodeToNameMap.get(code)
+
+
+const usStateCodeToNameMap: Map<string, string> = new Map()
+
+usStates.forEach((usState: Place) => usStateCodeToNameMap.set(usState.code, usState.name))
+
+export const usStateCodeToName = (code: string): MaybeString =>
+  usStateCodeToNameMap.get(code)
