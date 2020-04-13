1116d85b9150f106ebc5fdb2c77c086908c6675f
Libby Li <libby@leapyear.io>
2020-04-12 18:20:55 -0700

    Remove required from Input

--- react_ui/src/components/Input.tsx
+++ react_ui/src/components/Input.tsx
@@ -17,7 +17,6 @@ export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({
       <input
         id={id}
         type={type}
-        required
         onChange={onChange}
         {...rest}
         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
