d160273147f908e1227bce79fa7da7b627c9efc5
bion <bionjohnson@gmail.com>
2020-04-12 22:19:32 -0700

    Display USA for US country name

--- react_ui/src/util/placeCodeToName.ts
+++ react_ui/src/util/placeCodeToName.ts
@@ -10,15 +10,20 @@ type Place = {
 
 const countryCodeToNameMap: Map<string, string> = new Map()
 
-countries.forEach((country: Place) => countryCodeToNameMap.set(country.code, country.name))
+countries.forEach((country: Place) =>
+  countryCodeToNameMap.set(country.code, country.name)
+)
+
+countryCodeToNameMap.set('US', 'USA')
 
 export const countryCodeToName = (code: string): MaybeString =>
   countryCodeToNameMap.get(code)
 
-
 const usStateCodeToNameMap: Map<string, string> = new Map()
 
-usStates.forEach((usState: Place) => usStateCodeToNameMap.set(usState.code, usState.name))
+usStates.forEach((usState: Place) =>
+  usStateCodeToNameMap.set(usState.code, usState.name)
+)
 
 export const usStateCodeToName = (code: string): MaybeString =>
   usStateCodeToNameMap.get(code)
