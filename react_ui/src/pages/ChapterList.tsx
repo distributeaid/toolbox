db8d5bf14f4055622a3129a23a07d9578679d1fc
David Golightly <david.golightly@leapyear.io>
2020-04-13 19:45:40 -0700

    basic hover shadow on list item

--- react_ui/src/pages/ChapterList.tsx
+++ react_ui/src/pages/ChapterList.tsx
@@ -7,6 +7,7 @@ import { ContentContainer } from '../components/ContentContainer'
 import { FAQFooter } from '../components/FAQFooter'
 import { MainContent } from '../components/MainContent'
 import { MainHeader } from '../components/MainHeader'
+import { P } from '../components/P'
 import { PreHeader } from '../components/PreHeader'
 import { useGetChapterListQuery } from '../generated/graphql'
 
@@ -17,7 +18,7 @@ export const ChapterItem: React.FC<{
     <li className="border-t border-gray-200 bg-white font-heading">
       <NavLink
         to={`/${chapter.slug}`}
-        className="p-6 flex flex-row hover:bg-gray-200 text-black text-sm">
+        className="p-6 flex flex-row text-black text-sm hover:shadow-xl hover:relative hover:z-10">
         <div className="leading-5 font-semibold w-4/12">
           {chapter.location.province}
         </div>
@@ -56,14 +57,14 @@ export const ChapterList: React.FC = () => {
       </ContentContainer>
 
       <div className="border-gray-400 border-t w-full bg-blue-50 mt-16 py-16">
-        <ContentContainer>
+        <div className="container p-4 md:mx-auto w-full max-w-4xl">
           <div className="grid grid-cols-1 gap-4">
             <h3 className="font-heading text-3xl font-semibold px-6 pb-6">
               United States
             </h3>
             <div className="flex px-6 font-heading font-semibold text-lg text-blue-600">
               <div className="w-4/12 pb-0">{t('chapterList.provinceTerm')}</div>
-              <div className="w-6/12 pb-0">{t('chapterList.coverageArea')}</div>
+              <div className="w-6/12 pb-0">{t('chapterList.chapterName')}</div>
             </div>
             <ul>
               {chapters.map(
@@ -71,7 +72,11 @@ export const ChapterList: React.FC = () => {
               )}
             </ul>
           </div>
-        </ContentContainer>
+
+          <P className="text-center mt-8 mx-auto w-1/2">
+            <Trans i18nKey="chapterList.cta" />
+          </P>
+        </div>
         <FAQFooter />
       </div>
     </MainContent>
