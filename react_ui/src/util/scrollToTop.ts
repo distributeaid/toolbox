151138c641e78e074dd2f641324214b8ab3c6d97
Ayush Kumar <ayushkumar26@gmail.com>
2020-04-14 20:38:40 -0700

    make scroll to top a function

--- react_ui/src/util/scrollToTop.ts
+++ react_ui/src/util/scrollToTop.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
-const ScrollToTop: React.FunctionComponent = () => {
+const ScrollToTop = () => {
   const { pathname } = useLocation()
 
   useEffect(() => {
