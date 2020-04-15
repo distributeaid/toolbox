f0153727cb4ef305c0f7fd512228f78820acc585
jefk <jeff@substantial.com>
2020-04-15 14:04:54 -0700

    use react helmet for og tags

--- react_ui/src/App.tsx
+++ react_ui/src/App.tsx
@@ -3,6 +3,7 @@ import '@aws-amplify/ui/dist/style.css'
 
 import { ApolloProvider } from '@apollo/client'
 import React, { Suspense } from 'react'
+import { Helmet } from 'react-helmet'
 import {
   BrowserRouter as Router,
   Redirect,
@@ -17,6 +18,7 @@ import { useAuthState } from './auth/useAuthState'
 import { Footer } from './components/layout/Footer'
 import { NavBar } from './components/layout/NavBar'
 import { SubNav } from './components/layout/SubNav'
+import ogImage from './og/image.png'
 import AuthenticatorWrapper from './pages/Authenticator'
 import { Chapter } from './pages/Chapter'
 import { ChapterEdit } from './pages/ChapterEdit'
@@ -30,6 +32,14 @@ const App: React.FunctionComponent = () => {
 
   return (
     <ApolloProvider client={client}>
+      <Helmet>
+        <meta property="og:site_name" content="Masks For Docs"></meta>
+        <meta property="og:title" content="Masks For Docs"></meta>
+        <meta
+          property="og:image"
+          content={[process.env.PUBLIC_URL, ogImage].join('')}
+        />
+      </Helmet>
       <Suspense fallback="Loading...">
         <Router>
           <ScrollToTop />
