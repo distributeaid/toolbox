b6c9a163177347b527e07aae58e08b10876c7b9a
jefk <jeff@substantial.com>
2020-04-22 11:50:10 -0700

    remove existing auth

--- lib/ferry/accounts/user.ex
+++ lib/ferry/accounts/user.ex
@@ -18,6 +18,7 @@ defmodule Ferry.Accounts.User do
     user
     |> cast(attrs, [:email])
     |> validate_required([:email])
+    |> validate_length(:email, min: 5, max: 255)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint(:email)
   end
