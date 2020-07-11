34b993e56dcadc94f83993e6e0f5c99e2d00d362
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-07-11 07:57:23 +0200

    docs and typespecs

--- lib/ferry_api/category_schema.ex
+++ lib/ferry_api/category_schema.ex
@@ -1,7 +1,6 @@
 defmodule FerryApi.Schema.Category do
   use Absinthe.Schema.Notation
 
-  # alias AbsintheErrorPayload.ValidationMessage
   import AbsintheErrorPayload.Payload
   alias Ferry.AidTaxonomy
   alias FerryApi.Middleware
@@ -61,53 +60,90 @@ defmodule FerryApi.Schema.Category do
     end
   end
 
-  # Resolvers
-  # ------------------------------------------------------------
+  @category_not_found "category not found"
+
+  @doc """
+  Graphql resolver that returns the total number of categories
+  """
+  @spec count_categories(map(), map(), Absinthe.Resolution.t()) :: {:ok, non_neg_integer()}
   def count_categories(_parent, _args, _resolution) do
     {:ok, length(AidTaxonomy.list_categories())}
   end
 
+  @doc """
+  Graphql resolver that returns a collection of entities
+  """
+  @spec list_categories(map(), map(), Absinthe.Resolution.t()) ::
+          {:ok, [AidTaxonomy.Category.t()]}
   def list_categories(_parent, _args, _resolution) do
     {:ok, AidTaxonomy.list_categories()}
   end
 
+  @doc """
+  Graphql resolver that returns a single category, given its id
+  """
+  @spec get_category(any, %{id: integer}, any) ::
+          {:error, String.t()} | {:ok, Ferry.AidTaxonomy.Category.t()}
   def get_category(_parent, %{id: id}, _resolution) do
     case AidTaxonomy.get_category(id) do
       nil ->
-        {:error, "category not found"}
+        {:error, @category_not_found}
 
       category ->
         {:ok, category}
     end
   end
 
+  @doc """
+  Graphql resolver that returns a single category, given its name
+  """
+  @spec get_category_by_name(any, %{name: binary}, any) ::
+          {:error, String.t()} | {:ok, Ferry.AidTaxonomy.Category.t()}
   def get_category_by_name(_parent, %{name: name}, _resolution) do
     case AidTaxonomy.get_category_by_name(name) do
       nil ->
-        {:error, "category not found"}
+        {:error, @category_not_found}
 
       category ->
         {:ok, category}
     end
   end
 
+  @doc """
+  Graphql resolver that creates a category
+  """
+  @spec create_category(
+          any,
+          %{category_input: map()},
+          any
+        ) :: {:error, Ecto.Changeset.t()} | {:ok, Ferry.AidTaxonomy.Category.t()}
   def create_category(_parent, %{category_input: category_attrs}, _resolution) do
     AidTaxonomy.create_category(category_attrs)
   end
 
+  @doc """
+  Graphql resolver that updates an existing category
+  """
+  @spec update_category(any, %{category_input: any, id: integer}, any) ::
+          {:error, String.t() | Ecto.Changeset.t()} | {:ok, Ferry.AidTaxonomy.Category.t()}
   def update_category(_parent, %{id: id, category_input: category_attrs}, _resolution) do
     case AidTaxonomy.get_category(id) do
       nil ->
-        {:error, "category not found"}
+        {:error, @category_not_found}
 
       category ->
         AidTaxonomy.update_category(category, category_attrs)
     end
   end
 
+  @doc """
+  Graphql resolver that deletes an existing category
+  """
+  @spec delete_category(any, %{id: integer}, any) ::
+          {:error, String.t() | Ecto.Changeset.t()} | {:ok, Ferry.AidTaxonomy.Category.t()}
   def delete_category(_parent, %{id: id}, _resolution) do
     case AidTaxonomy.get_category(id) do
-      nil -> {:error, "category not found"}
+      nil -> {:error, @category_not_found}
       category -> AidTaxonomy.delete_category(category)
     end
   end
