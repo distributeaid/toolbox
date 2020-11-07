eefbf74da1bb314f82f4eafbae8cc714dc463f81
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-11-07 21:50:42 +0100

    warn if missing jwt secret

--- lib/ferry/token.ex
+++ lib/ferry/token.ex
@@ -1,29 +1,26 @@
 defmodule Ferry.Token do
   use Joken.Config
+  require Logger
 
   def setup() do
     token_config = Application.get_env(:ferry, Ferry.Token)
 
     pem = token_config[:key]
 
-    if pem == "" or pem == nil do
-      raise "Missing :key in Ferry.Token: #{
-              inspect(
-                config: token_config,
-                audience: System.get_env("JWT_AUDIENCE"),
-                issuer: System.get_env("JWT_ISSUER"),
-                key: System.get_env("JWT_SECRET"),
-                app_env: Application.get_all_env(:ferry),
-                os_env: System.get_env()
-              )
-            }"
-    end
+    pem =
+      case pem do
+        nil ->
+          Logger.warn("Missing JWT secret in #{inspect(token_config)}")
+
+        _ ->
+          Base.decode64!(pem)
+      end
 
     Application.put_env(
       :joken,
       :default_signer,
       signer_alg: "RS256",
-      key_pem: Base.decode64!(pem)
+      key_pem: pem
     )
   end
 
