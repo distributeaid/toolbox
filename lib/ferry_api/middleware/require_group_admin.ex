b66e5559962706b4d3f89fc521300ce5df2645fc
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-11-18 01:15:27 +0100

    docs

--- lib/ferry_api/middleware/require_group_admin.ex
+++ lib/ferry_api/middleware/require_group_admin.ex
@@ -3,8 +3,8 @@ defmodule FerryApi.Middleware.RequireGroupAdmin do
   Simple GraphQL middleware that checks whether the current
   user is an admin in the group found in the arguments.
 
-  This middleware may be reused in multiple resolvers by
-  adding extra function clauses in order to decide where
+  This middleware may be reused in multiple resolvers if necessary
+  by adding extra function clauses, in order to decide where
   in the current scope the relevant group should be taken from.
   """
   @behaviour Absinthe.Middleware
